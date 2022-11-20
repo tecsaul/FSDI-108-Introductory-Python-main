bbc03abdc74909726be44662003adb8e314f1244
saul nuñez <77266273+tecsaul@users.noreply.github.com>
2022-11-19 17:35:04 -0800

    Add files via upload

---
+++ FSDI-108-Introductory-Python-main/config.py
@@ -0,0 +1,14 @@
+
+
+me = {
+    'first': 'Saul',
+    'last': "Nuñez",
+    'age': 35,
+    'hobbies': ['music', 'movies', 'sports'],
+    'address': {
+        'street': 'Main St',
+        'number': 345,
+        'city': 'New York',
+    }
+
+}
